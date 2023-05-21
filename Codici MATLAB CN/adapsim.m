ff6987063aae1c72ff30087894631c0f78b16bef
Alexdeci <alexdecicco1999@gmail.com>
2023-05-21 23:24:59 +0200

    aggiunti adapsim e adapquad per l'esercizio 28-29

--- Codici MATLAB CN/adapsim.m
+++ Codici MATLAB CN/adapsim.m
@@ -16,26 +16,30 @@ elseif a>b
 elseif tol<0
     error('tolleranza negativa');
 end
+fe=0;
+I2=0;
 x1=(a+b)/2;
+fe=0;
 if nargin==4
     fa=feval(f,a);
     fb=feval(f,b);
     f1=feval(f,x1);
-    vf=3;
+    fe=3;
 end
 h=(b-a)/6;
 I1=h*(fa+4*f1+fb);
 x2=(a+x1)/2;
-x3(x1+b)/2;
+x3=(x1+b)/2;
 f2=feval(f,x2);
 f3=feval(f,x3);
 I2=.5*h*(fa+4*f2+2*f1+4*f3+fb);
-vf=vf+2;
+fe=fe+2;
 e=abs(I2-I1)/15;
 if e>tol
-    [left,vf]=adapsim(a,x1,f,tol/2,fa,f2,f1);
-    [right,vf]=adapsim(x1,b,f,tol/2,f1,f3,fb);
+    [left,fe1]=adapsim(a,x1,f,tol/2,fa,f2,f1);
+    [right,fe2]=adapsim(x1,b,f,tol/2,f1,f3,fb);
     I2=left+right;
+    fe=fe+fe1+fe2;
 end
 return
 end
